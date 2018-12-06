24abf186fcc86338e2c866249d34606996f5222b
AkshayaPk <ashpeekay23@gmail.com>
2018-12-06 05:42:59 -0500

    added 1 endpoint

--- src/main/java/com/revature/sample/SampleApplication.java
+++ src/main/java/com/revature/sample/SampleApplication.java
@@ -2,11 +2,20 @@ package com.revature.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+	
+	@GetMapping("/home")
+	public String display() {
+		return "Hello";
 	}
 }
