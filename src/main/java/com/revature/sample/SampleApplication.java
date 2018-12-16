42caf4f059094febf61090d3b16163def395fe7c
AkshayaPk <ashpeekay23@gmail.com>
2018-12-16 10:22:04 -0500

    Update SampleApplication.java

--- src/main/java/com/revature/sample/SampleApplication.java
+++ src/main/java/com/revature/sample/SampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
-import java.lang.*
+import java.lang.*;
 
 @SpringBootApplication
 @RestController
